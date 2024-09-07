f8958f9715c249c4a74e0c93e774e51bc9084f16
Aditya-sairam <adityasairam95@gmail.com>
2024-09-06 21:17:02 -0400

    Lab-1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,10 @@ function Lab1() {
     return (
         <div>
             <h2> Lab1</h2>
+            <h3>Exercise 1.1</h3>
+            <h3>Exercise 1.2</h3>
+            <h3>Exercise 1.3</h3>
+
         </div>
     )
 }
