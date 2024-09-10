8a2c1b479465f2e19a49a35e446f686179e0de75
Aditya-sairam <adityasairam95@gmail.com>
2024-09-10 00:29:46 -0400

    Added Navigation for labs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Labs from "./Labs";
+import TOC from "./Labs/TOC";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function App(){
   return(
+    <HashRouter>
     <div>
-      <Labs />
+      <Routes>
+        <Route path="/" element={<TOC  />} />
+      <Route path="/Labs/*" element={<Labs />} />
+      </Routes>
     </div>
+    </HashRouter>
   )
 }
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,10 +1,22 @@
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+import TOC from "./TOC";
 
 export default function Labs() {
     return(
-        <div id="wd-Labs">
-            <h1>Labs</h1>
-            <Lab1 />
+        <div>
+             <h1>Labs</h1>
+            <Routes>
+        <Route path="/" element={<Navigate to="TOC" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Lab3" element={<Lab3 />} />
+        
+        
+        </Routes>
         </div>
+       
     )
 }
