8c8b2d3ec0b0753fab34ef95970e7be76f4ab965
Aditya-sairam <adityasairam95@gmail.com>
2024-09-14 07:40:59 -0400

    Working on Nav bard and dashboard

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,16 +1,25 @@
 import { Navigate, Route, Routes } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard/Dashboard";
+import KanbasNavigation from "./Navigation";
 
 export default function Kanbas () {
     return (
         <div id="wd-kanbas">
-            <h1>Kanbas</h1>
+            <table>            
+                <tr>
+                    <td valign="top">
+                        <KanbasNavigation />
+                    </td>
+            <td valign="top">
             <Routes>
-                <Route path="/" element={<Navigate to="Account" />} />
-                <Route path="/Account/*" element={<Account />} />
+                <Route path="/" element={<Navigate to="Account" />} /> 
+                <Route path="/Account/*" element={<Account />} /> 
                 <Route path="/Dashboard" element = {<Dashboard />} />
             </Routes>
+            </td>
+            </tr>
+            </table>
         </div>
     );
 }
